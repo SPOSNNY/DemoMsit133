c11b1f94f57e6932d42a130c8d88b580303e44d7
sposnny <sposnny@gmail.com>
2022-04-20 15:30:56 +0800

    加入專案檔案。

---
+++ DemoMsit133/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoMsit133.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
